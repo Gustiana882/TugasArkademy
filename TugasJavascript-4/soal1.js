dd07715857738b2165def804a5346d0fe4b9bc89
Gustiana882 <gustiana882@gmail.com>
2021-07-03 22:58:06 +0700

    delete folder tugas javascript

--- TugasJavascript-4/soal1.js
+++
@@ -1,9 +0,0 @@
-// palindrom
-
-const palindrom = (props) => {
-    let input = props.toLowerCase()
-    let text = input.split('').reverse().join('');
-    (text === input) ? console.log('palindrom') : console.log('bukan palindrom');
-}
-
-palindrom("Siang")
--- TugasJavascript-4/soal2.js
+++
@@ -1,6 +0,0 @@
-const reverseWord = (props) => {
-    let text = props.split(' ').reverse().join(' ');
-    console.log(text)
-}
-
-reverseWord("Saya Belajar Javascript")
